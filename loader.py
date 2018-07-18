ec1b7053497fa009689bf742e6227f683495c3f0
なるみ <weaper@gmail.com>
2018-07-18 18:52:54 +0800

    add loader.py

---
+++ loader.py
@@ -0,0 +1,16 @@
+from torch.utils import data
+from torchvision import datasets, transforms
+
+
+def get_dataloader(root='data', batch_size=64, num_workers=0, size=64):
+    transform = transforms.Compose(
+        [transforms.Resize(size),
+         transforms.ToTensor()])
+
+    dataloader = data.DataLoader(
+        datasets.MNIST(root, train=True, transform=transform),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers)
+
+    return dataloader
